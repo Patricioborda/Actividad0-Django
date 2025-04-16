70399b24c4b9ac6ce40c150064027855f367a07e
Patricioborda <patricio.borda@outlook.com>
2025-04-16 14:44:14 -0300

    [Parte 3.4][fix]Utilice el sistema de plantillas

--- mi_proyecto/mi_proyecto/urls.py
+++ mi_proyecto/mi_proyecto/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),  # Incluir las URLs de la aplicación polls
     path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),  # Para las rutas de la aplicación polls
+    path('', include('polls.urls')),  # Esto redirige la raíz '/' a las rutas de polls
 ]
