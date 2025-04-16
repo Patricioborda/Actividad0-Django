0be9cbae1b7567a5c4a4d3965ff9df6ef0b1a475
Patricioborda <patricio.borda@outlook.com>
2025-04-16 15:20:36 -0300

    [Parte 4.2] Modifique el URLconf

--- mi_proyecto/polls/urls.py
+++ mi_proyecto/polls/urls.py
@@ -5,11 +5,11 @@ app_name = "polls"  # Define el espacio de nombres para las URLs de esta app
 
 urlpatterns = [
     # ex: /polls/
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),  # Usa IndexView (ListView)
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),  # Usa DetailView
     # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),  # Usa ResultsView (también DetailView)
     # ex: /polls/5/vote/
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),  # Vista de voto sigue siendo una función
 ]
